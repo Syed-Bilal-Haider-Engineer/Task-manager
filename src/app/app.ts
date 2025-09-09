2e2f275ba05faab60c85ccda60d243e9ac269ae2
syed bilal haider <bilaldev151214@gmail.com>
2025-09-09 03:22:19 +0200

    setup user sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header';
+import { User } from "./user/user";
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -3,8 +3,8 @@ import {Component} from '@angular/core';
 
 @Component({
     selector:'app-header',
-    templateUrl:'./header.component.html',
-    styleUrls:['./header.component.css']
+    templateUrl:'./header.html',
+    styleUrls:['./header.css']
 }
 )
 
